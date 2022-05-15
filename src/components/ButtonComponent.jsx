1010f55bd721eece4c80cfbf8b9037762366971b
Karthik <smkarthik98@gmail.com>
2022-05-15 15:04:57 +0530

    connected to metamask

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const ButtonComponent = () => {
+  const [walletAddress, setWalletAddress] = useState("");
+  const handleConnect = async () => {
+    if (window.ethereum) {
+      console.log("Metamask detected");
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error fetching account");
+      }
+    } else {
+      alert("Metamask not detected");
+    }
+  };
+  return (
+    <>
+      <button onClick={handleConnect}>Connect to Wallet</button>
+      <h1>Your Meta Mask wallet address = {walletAddress}</h1>
+    </>
+  );
+};
